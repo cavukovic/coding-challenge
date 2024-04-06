1c3b2cd021485f026a7bd17c3444e63044d48bdc
Charlie Vukovic <cavukovic@gmail.com>
2024-04-06 12:18:32 -0400

    updates for deployment site

--- coding-challenge/src/App.jsx
+++ coding-challenge/src/App.jsx
@@ -6,7 +6,6 @@ import xcircle from "./assets/x-circle-fill.svg";
 const App = () => {
   const [authorName, setAuthorName] = useState("");
   const [topAuthors, setTopAuthors] = useState([]);
-  const [selectedAuthor, setSelectedAuthor] = useState(null);
   const [error, setError] = useState("");
 
   const handleAuthorNameChange = (event) => {
@@ -20,33 +19,46 @@ const App = () => {
   };
 
   const handleSearch = () => {
-    // Make an HTTP GET request to the server endpoint
-    fetch(`http://localhost:5174/top-authors?author_name=${authorName}`)
-      .then((response) => {
-        // Check if the response is successful (status code 2xx)
-        if (!response.ok) {
-          throw new Error("Failed to fetch top authors");
-        }
-        // Parse the JSON response
-        return response.json();
-      })
-      .then((data) => {
-        // Update the state with the fetched top authors
-        console.log("update top authors");
-        console.log(data);
-        setTopAuthors(data);
-        setError("");
-      })
-      .catch((error) => {
-        // Handle any errors that occur during the fetch operation
-        console.error("Error fetching top authors:", error);
-        setError("Failed to fetch top authors");
-        setTopAuthors([]);
-      });
-  };
-
-  const handleAuthorClick = (author) => {
-    setSelectedAuthor(author);
+    const currentURL = window.location.href;
+    if (currentURL == "http://localhost:5173/") {
+      console.log("currentURL:", currentURL);
+      // Make an HTTP GET request to the server endpoint
+      fetch(`http://localhost:5174/top-authors?author_name=${authorName}`)
+        .then((response) => {
+          // Check if the response is successful (status code 2xx)
+          if (!response.ok) {
+            throw new Error("Failed to fetch top authors");
+          }
+          // Parse the JSON response
+          return response.json();
+        })
+        .then((data) => {
+          // Update the state with the fetched top authors
+          setTopAuthors(data);
+          setError("");
+        })
+        .catch((error) => {
+          // Handle any errors that occur during the fetch operation
+          console.error("Error fetching top authors:", error);
+          setError("Failed to fetch top authors");
+          setTopAuthors([]);
+        });
+    } else {
+      const dummyData = [
+        { name: "Alice Johnson", total_revenue: 106.5 },
+        { name: "Matthew Hernandez", total_revenue: 99.75 },
+        { name: "Laura Flores", total_revenue: 88.5 },
+        { name: "Jessica Martinez", total_revenue: 86.97 },
+        { name: "David Lee", total_revenue: 83.4 },
+        { name: "Emily Taylor", total_revenue: 60 },
+        { name: "John Doe", total_revenue: 51.98 },
+        { name: "Michael Brown", total_revenue: 45 },
+        { name: "Daniel Rodriguez", total_revenue: 43 },
+        { name: "Amanda Gonzalez", total_revenue: 40 },
+      ];
+      setTopAuthors(dummyData);
+      setError("");
+    }
   };
 
   return (
@@ -78,7 +90,7 @@ const App = () => {
             </div>
             <ul className="member-list">
               {topAuthors.map((author, index) => (
-                <li key={index} onClick={() => handleAuthorClick(author)}>
+                <li key={index}>
                   <div className="name-and-text">
                     <img
                       height="60px"
@@ -96,12 +108,6 @@ const App = () => {
                 </li>
               ))}
             </ul>
-          </div>
-        )}
-        {selectedAuthor && (
-          <div>
-            <h2>{selectedAuthor.name}</h2>
-            <p>Total Revenue: {selectedAuthor.total_revenue}</p>
           </div>
         )}
       </div>
